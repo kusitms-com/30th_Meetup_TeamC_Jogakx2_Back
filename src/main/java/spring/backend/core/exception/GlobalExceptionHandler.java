d148c168707c1bf61d429fe56da2091a5ed7d02e
anxi01 <tjdals2893@naver.com>
2024-10-11 05:02:42 +0900

    fix: (#11) 에러 응답을 ResponseEntity로 변경하여 상태 코드 처리 방식을 변경한다

--- src/main/java/spring/backend/core/exception/GlobalExceptionHandler.java
+++ src/main/java/spring/backend/core/exception/GlobalExceptionHandler.java
@@ -3,8 +3,8 @@ package spring.backend.core.exception;
 import java.util.Optional;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -15,16 +15,17 @@ import spring.backend.core.presentation.ErrorResponse;
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(Exception.class)
-  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-  public final ErrorResponse handleAllExceptions(Exception ex, WebRequest request) {
-    logger.error("ERROR ::: [AllException] ", ex);
-    return ErrorResponse.of(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+  public final ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex, WebRequest request) {
+    log.error("ERROR ::: [AllException] ", ex);
+    ErrorResponse errorResponse = ErrorResponse.createErrorResponse().statusCode(500).exception(ex).build();
+    return ResponseEntity.internalServerError().body(errorResponse);
   }
 
   @ExceptionHandler(DomainException.class)
-  public final ErrorResponse handleDomainException(DomainException ex) {
+  public final ResponseEntity<ErrorResponse> handleDomainException(DomainException ex) {
     HttpStatus httpStatus = Optional.ofNullable(ex.getHttpStatus()).orElse(HttpStatus.INTERNAL_SERVER_ERROR);
     log.error("ERROR ::: [DomainException] ", ex);
-    return ErrorResponse.of(ex.getMessage(), httpStatus);
+    ErrorResponse errorResponse = ErrorResponse.createDomainErrorResponse().statusCode(httpStatus.value()).exception(ex).build();
+    return ResponseEntity.status(httpStatus).body(errorResponse);
   }
 }

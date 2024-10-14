31cbe127ac1e0ecb4f2d06f9e81f12ac373f08bb
HoyeongJeon <hoyeong0302@gmail.com>
2024-10-15 01:41:29 +0900

    feat: (#18) Access Token , RefeshToken 생성 서비스를 Static 메서드로 수정한다.

--- src/main/java/spring/backend/auth/application/AccessTokenProviderService.java
+++ src/main/java/spring/backend/auth/application/AccessTokenProviderService.java
@@ -1,9 +1,34 @@
 package spring.backend.auth.application;
 
+import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import spring.backend.auth.domain.jwt.value.Type;
+import spring.backend.member.domain.entity.Member;
+
+import javax.crypto.SecretKey;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 @Slf4j
 @Service
 public class AccessTokenProviderService {
+    private static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+
+    public static String accessTokenProvider(Member member) {
+        // 기한 설정 , 지금부터 1일
+        Date expiryDate = Date.from(
+                Instant.now().plus(1, ChronoUnit.DAYS)
+        );
+
+        // AccessToken 생성
+        return Jwts.builder()
+                .subject(member.getEmail()) // 추후 변경 가능
+                .claim("type", Type.ACCESS.getType())
+                .issuedAt(new Date())
+                .expiration(expiryDate)
+                .signWith(SECRET_KEY)
+                .compact();
+    }
 }

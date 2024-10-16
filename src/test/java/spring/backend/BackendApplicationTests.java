53bd1bcfa0332531299090746c5be9dda39c99ed
anxi01 <tjdals2893@naver.com>
2024-10-17 00:21:15 +0900

    chore: (#24) test 환경의 property의 profiles를 제거한다

--- src/test/java/spring/backend/BackendApplicationTests.java
+++ src/test/java/spring/backend/BackendApplicationTests.java
@@ -2,10 +2,8 @@ package spring.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("local")
 class BackendApplicationTests {
 
 	@Test

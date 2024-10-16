0a5ec9d8213f8945ae8577886a6c60f6025b7367
anxi01 <tjdals2893@naver.com>
2024-10-17 00:26:11 +0900

    feat: (#24) Authorization 인터셉터를 추가한다

---
+++ src/main/java/spring/backend/core/configuration/WebMvcConfiguration.java
@@ -0,0 +1,19 @@
+package spring.backend.core.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import spring.backend.core.configuration.interceptor.AuthorizationInterceptor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    private final AuthorizationInterceptor authorizationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authorizationInterceptor);
+    }
+}

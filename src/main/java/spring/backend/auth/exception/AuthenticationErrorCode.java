77914fe70beee1c1bf51cb3dcdea2974bb3638f3
anxi01 <tjdals2893@naver.com>
2024-10-16 23:25:21 +0900

    fix: (#24) AuthenticationException을 DomainException으로 변경한다

--- src/main/java/spring/backend/auth/exception/AuthenticationErrorCode.java
+++ src/main/java/spring/backend/auth/exception/AuthenticationErrorCode.java
@@ -3,17 +3,18 @@ package spring.backend.auth.exception;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import spring.backend.core.exception.AuthenticationException;
+import spring.backend.core.exception.DomainException;
 import spring.backend.core.exception.error.BaseErrorCode;
 
 @Getter
 @RequiredArgsConstructor
-public enum AuthenticationErrorCode implements BaseErrorCode<AuthenticationException> {
+public enum AuthenticationErrorCode implements BaseErrorCode<DomainException> {
 
     NOT_EXIST_HEADER(HttpStatus.UNAUTHORIZED, "Authorization Header가 존재하지 않습니다."),
     NOT_EXIST_TOKEN(HttpStatus.UNAUTHORIZED, "Authorization Header에 Token이 존재하지 않습니다."),
     NOT_MATCH_TOKEN_FORMAT(HttpStatus.UNAUTHORIZED, "토큰의 형식이 맞지 않습니다."),
     NOT_DEFINE_TOKEN(HttpStatus.UNAUTHORIZED, "정의되지 않은 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
     INVALID_PROVIDER(HttpStatus.BAD_REQUEST, "유효한 OAuth 써드파티 제공자가 아닙니다."),
     NOT_EXIST_PROVIDER(HttpStatus.BAD_REQUEST, "OAuth 써드파티 제공자가 존재하지 않습니다."),
     NOT_EXIST_AUTH_CODE(HttpStatus.BAD_GATEWAY, "OAuth 써드파티 제공자에서 제공받은 인증 코드가 존재하지 않습니다."),
@@ -26,7 +27,7 @@ public enum AuthenticationErrorCode implements BaseErrorCode<AuthenticationExcep
     private final String message;
 
     @Override
-    public AuthenticationException toException() {
-        return new AuthenticationException(message);
+    public DomainException toException() {
+        return new DomainException(httpStatus, this);
     }
 }

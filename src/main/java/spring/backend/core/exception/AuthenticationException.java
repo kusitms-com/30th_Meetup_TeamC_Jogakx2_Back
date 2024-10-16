287082e1b2e0ff4dce32467aad2cc69c0165fe2c
anxi01 <tjdals2893@naver.com>
2024-10-16 23:25:21 +0900

    fix: (#24) AuthenticationException을 DomainException으로 변경한다

--- src/main/java/spring/backend/core/exception/AuthenticationException.java
+++
@@ -1,8 +0,0 @@
-package spring.backend.core.exception;
-
-public class AuthenticationException extends RuntimeException {
-
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}

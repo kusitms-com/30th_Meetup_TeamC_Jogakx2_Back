33d0214f0216099d412cd690e49e3f0fdda7e062
HoyeongJeon <hoyeong0302@gmail.com>
2024-10-18 01:49:30 +0900

    feat: (#28) RedisService에 에러처리를 추가한다.

--- src/main/java/spring/backend/core/application/RedisService.java
+++ src/main/java/spring/backend/core/application/RedisService.java
@@ -1,7 +1,9 @@
 package spring.backend.core.application;
 
+import io.lettuce.core.RedisConnectionException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import spring.backend.core.exception.error.GlobalErrorCode;
 import spring.backend.core.infrastructure.redis.repository.RedisRepository;
 
 import java.time.temporal.ChronoUnit;
@@ -14,15 +16,27 @@ public class RedisService {
     private final RedisRepository redisRepository;
 
     public void saveRefreshToken(UUID memberId, String refreshToken, Long expireTime, ChronoUnit chronoUnit) {
-        TimeUnit timeUnit = convertChronoUnitToTimeUnit(chronoUnit);
-        redisRepository.saveRefreshToken(memberId, refreshToken, expireTime, timeUnit);
+        try {
+            TimeUnit timeUnit = convertChronoUnitToTimeUnit(chronoUnit);
+            redisRepository.saveRefreshToken(memberId, refreshToken, expireTime, timeUnit);
+        } catch (RedisConnectionException e) {
+            throw GlobalErrorCode.REDIS_CONNECTION_ERROR.toException();
+        } catch (Exception e) {
+            throw GlobalErrorCode.INTERNAL_ERROR.toException();
+        }
     }
 
     public String getRefreshToken(UUID memberId) {
-        return redisRepository.getRefreshToken(memberId);
+        try {
+            return redisRepository.getRefreshToken(memberId);
+        } catch (RedisConnectionException e) {
+            throw GlobalErrorCode.REDIS_CONNECTION_ERROR.toException();
+        } catch (Exception e) {
+            throw GlobalErrorCode.INTERNAL_ERROR.toException();
+        }
     }
 
-    private TimeUnit convertChronoUnitToTimeUnit(ChronoUnit chronoUnit) {
+    public TimeUnit convertChronoUnitToTimeUnit(ChronoUnit chronoUnit) {
         switch (chronoUnit) {
             case NANOS:
                 return TimeUnit.NANOSECONDS;
@@ -39,7 +53,7 @@ public class RedisService {
             case DAYS:
                 return TimeUnit.DAYS;
             default:
-                throw new UnsupportedOperationException("Unsupported ChronoUnit: " + chronoUnit);
+                throw GlobalErrorCode.UNSUPPORTED_TYPE.toException();
         }
-    };
+    }
 }

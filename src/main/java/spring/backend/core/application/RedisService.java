c21b3a52a58983e30db9bb3f9667286b4fac2b6f
HoyeongJeon <hoyeong0302@gmail.com>
2024-10-17 20:28:26 +0900

    feat: (#28) RefreshToken을 Redis에 저장한다.

--- src/main/java/spring/backend/core/application/RedisService.java
+++ src/main/java/spring/backend/core/application/RedisService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import spring.backend.core.infrastructure.redis.repository.RedisRepository;
 
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -11,15 +13,33 @@ import java.util.concurrent.TimeUnit;
 public class RedisService {
     private final RedisRepository redisRepository;
 
-    public void save(String key, String value) {
-        redisRepository.save(key, value);
+    public void saveRefreshToken(UUID memberId, String refreshToken, Long expireTime, ChronoUnit chronoUnit) {
+        TimeUnit timeUnit = convertChronoUnitToTimeUnit(chronoUnit);
+        redisRepository.saveRefreshToken(memberId, refreshToken, expireTime, timeUnit);
     }
 
-    public void save(String key, String value, Long expireTime, TimeUnit timeUnit) {
-        redisRepository.save(key, value, expireTime, timeUnit);
+    public String getRefreshToken(UUID memberId) {
+        return redisRepository.getRefreshToken(memberId);
     }
 
-    public String get(String key) {
-        return redisRepository.get(key);
-    }
+    private TimeUnit convertChronoUnitToTimeUnit(ChronoUnit chronoUnit) {
+        switch (chronoUnit) {
+            case NANOS:
+                return TimeUnit.NANOSECONDS;
+            case MICROS:
+                return TimeUnit.MICROSECONDS;
+            case MILLIS:
+                return TimeUnit.MILLISECONDS;
+            case SECONDS:
+                return TimeUnit.SECONDS;
+            case MINUTES:
+                return TimeUnit.MINUTES;
+            case HOURS:
+                return TimeUnit.HOURS;
+            case DAYS:
+                return TimeUnit.DAYS;
+            default:
+                throw new UnsupportedOperationException("Unsupported ChronoUnit: " + chronoUnit);
+        }
+    };
 }

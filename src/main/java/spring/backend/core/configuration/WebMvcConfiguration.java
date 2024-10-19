802171f707589916b68a918c03c730fec194b838
anxi01 <tjdals2893@naver.com>
2024-10-19 14:43:19 +0900

    feat: (#32) ArgumentResolver를 이용해 토큰을 멤버로 바인딩할 수 있다

--- src/main/java/spring/backend/core/configuration/WebMvcConfiguration.java
+++ src/main/java/spring/backend/core/configuration/WebMvcConfiguration.java
@@ -2,9 +2,13 @@ package spring.backend.core.configuration;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import spring.backend.core.configuration.argumentresolver.LoginMemberArgumentResolver;
 import spring.backend.core.configuration.interceptor.AuthorizationInterceptor;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -12,8 +16,15 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     private final AuthorizationInterceptor authorizationInterceptor;
 
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authorizationInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginMemberArgumentResolver);
     }
 }

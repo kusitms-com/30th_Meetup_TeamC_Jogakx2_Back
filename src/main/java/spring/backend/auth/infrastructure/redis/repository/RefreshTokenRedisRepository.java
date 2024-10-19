261677f7a6027d9b3c20c549fddbd03d2e98e888
HoyeongJeon <hoyeong0302@gmail.com>
2024-10-19 21:35:59 +0900

    fix : (#28) RefreshTokenRepository 관련 인터페이스와 구현체를 만듭니다.

---
+++ src/main/java/spring/backend/auth/infrastructure/redis/repository/RefreshTokenRedisRepository.java
@@ -0,0 +1,28 @@
+package spring.backend.auth.infrastructure.redis.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+import spring.backend.auth.domain.repository.RefreshTokenRepository;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@Repository
+@RequiredArgsConstructor
+public class RefreshTokenRedisRepository implements RefreshTokenRepository {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Override
+    public void save(UUID memberId, String refreshToken, Long expireTime, TimeUnit timeUnit) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(memberId.toString(), refreshToken, expireTime, timeUnit);
+    }
+
+    @Override
+    public String findById(UUID memberId) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(memberId.toString());
+    }
+}

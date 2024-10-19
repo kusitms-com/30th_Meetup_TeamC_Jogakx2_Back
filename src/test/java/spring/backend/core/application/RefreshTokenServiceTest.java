b7c41a1504abca7bc684649882f87727e93fe153
HoyeongJeon <hoyeong0302@gmail.com>
2024-10-19 21:38:11 +0900

    fix : (#28) RefreshTokenServiceTest를 작성합니다.

--- src/test/java/spring/backend/core/application/RefreshTokenServiceTest.java
+++ src/test/java/spring/backend/core/application/RefreshTokenServiceTest.java
@@ -1,12 +1,13 @@
 package spring.backend.core.application;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import spring.backend.auth.application.RefreshTokenService;
 import spring.backend.member.domain.entity.Member;
 
 import java.util.UUID;
@@ -14,14 +15,25 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class RedisServiceTest {
+public class RefreshTokenServiceTest {
     @Autowired
-    private RedisService redisService;
-
-    @Autowired
-    private JwtService jwtService;
+    private RefreshTokenService refreshTokenService;
 
     private final UUID memberId = UUID.randomUUID();
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @BeforeEach
+    public void checkRedisConnection() {
+        try {
+            redisTemplate.getConnectionFactory().getConnection();
+            System.out.println("Redis 연결 성공");
+        } catch (RedisConnectionFailureException e) {
+            System.err.println("Redis 연결 실패: " + e.getMessage());
+        }
+    }
+
 
     @AfterAll
     static void afterAll(@Qualifier("redisConnectionFactory") LettuceConnectionFactory connectionFactory) {
@@ -36,9 +48,7 @@ public class RedisServiceTest {
                 .id(memberId)
                 .email("test@test.com")
                 .build();
-        // when
-        String refreshToken = jwtService.provideRefreshToken(member);
-        // then
-        assertThat(refreshToken).isEqualTo(redisService.getRefreshToken(member.getId()));
+        // when & then
+        assertThat(refreshTokenService.saveRefreshToken(member)).isEqualTo(refreshTokenService.getRefreshToken(memberId));
     }
 }

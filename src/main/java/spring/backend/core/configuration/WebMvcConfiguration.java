ebd45920d70a92bee9868555068e4308c505817f
anxi01 <tjdals2893@naver.com>
2024-10-21 01:01:50 +0900

    feat: (#42) 프론트 도메인에 CORS를 허용한다

--- src/main/java/spring/backend/core/configuration/WebMvcConfiguration.java
+++ src/main/java/spring/backend/core/configuration/WebMvcConfiguration.java
@@ -3,6 +3,7 @@ package spring.backend.core.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import spring.backend.core.configuration.argumentresolver.LoginMemberArgumentResolver;
@@ -17,6 +18,15 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     private final AuthorizationInterceptor authorizationInterceptor;
 
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://cnergy.kro.kr")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {

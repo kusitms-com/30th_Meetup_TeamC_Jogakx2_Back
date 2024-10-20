0ab495512ee8d10a7ed7420c1914ed63c7212443
HoyeongJeon <hoyeong0302@gmail.com>
2024-10-20 15:44:55 +0900

    feat: (#35) RefreshTokenService의 Test를 작성한다.

--- src/main/java/spring/backend/auth/application/RefreshTokenService.java
+++ src/main/java/spring/backend/auth/application/RefreshTokenService.java
@@ -1,11 +1,13 @@
 package spring.backend.auth.application;
 
 import io.lettuce.core.RedisConnectionException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import spring.backend.auth.domain.repository.RefreshTokenRepository;
 import spring.backend.auth.exception.AuthenticationErrorCode;
 import spring.backend.core.application.JwtService;
+import spring.backend.core.exception.DomainException;
 import spring.backend.core.exception.error.GlobalErrorCode;
 import spring.backend.member.domain.entity.Member;
 
@@ -13,6 +15,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 public class RefreshTokenService {
     private final JwtService jwtService;
@@ -38,7 +41,24 @@ public class RefreshTokenService {
 
     public String getRefreshToken(UUID memberId) {
         try {
-            return refreshTokenRepository.findByMemberId(memberId);
+            String refreshToken =  refreshTokenRepository.findByMemberId(memberId);
+            if (refreshToken == null || refreshToken.isEmpty()) {
+                log.error("리프레시 토큰이 저장소에 존재하지 않습니다.");
+                throw AuthenticationErrorCode.NOT_EXIST_REFRESH_TOKEN.toException();
+            }
+            return refreshToken;
+        } catch (RedisConnectionException e) {
+            throw GlobalErrorCode.REDIS_CONNECTION_ERROR.toException();
+        } catch (DomainException e) {
+            throw e;
+        } catch (Exception e) {
+            throw GlobalErrorCode.INTERNAL_ERROR.toException();
+        }
+    }
+
+    public void deleteRefreshToken(UUID memberId) {
+        try {
+            refreshTokenRepository.delete(memberId);
         } catch (RedisConnectionException e) {
             throw GlobalErrorCode.REDIS_CONNECTION_ERROR.toException();
         } catch (Exception e) {

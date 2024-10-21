2f33df8aa78d2e1964fb43be0dcd28431946fbe6
anxi01 <tjdals2893@naver.com>
2024-10-21 17:28:50 +0900

    feat: (#44) 닉네임 검증 스웨거를 추가한다

--- src/main/java/spring/backend/member/presentation/ValidateNicknameController.java
+++ src/main/java/spring/backend/member/presentation/ValidateNicknameController.java
@@ -3,19 +3,19 @@ package spring.backend.member.presentation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import spring.backend.core.presentation.RestResponse;
 import spring.backend.member.application.ValidateNicknameService;
+import spring.backend.member.presentation.swagger.ValidateNicknameSwagger;
 
 @RestController
 @RequiredArgsConstructor
-public class ValidateNicknameController {
+public class ValidateNicknameController implements ValidateNicknameSwagger {
 
     private final ValidateNicknameService validateNicknameService;
 
     @GetMapping("/v1/members/check-nickname")
-    public ResponseEntity<RestResponse<Boolean>> validateNickname(@RequestParam String nickname) {
+    public ResponseEntity<RestResponse<Boolean>> validateNickname(String nickname) {
         boolean isValidNickname = validateNicknameService.validateNickname(nickname);
         return ResponseEntity.ok(new RestResponse<>(isValidNickname));
     }

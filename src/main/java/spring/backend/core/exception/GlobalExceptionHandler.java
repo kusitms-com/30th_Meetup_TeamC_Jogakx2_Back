9403f737fc8bfd0f3c1074251dc673660a39e87b
anxi01 <tjdals2893@naver.com>
2024-10-22 18:41:25 +0900

    feat: (#34) Spring Validation에서 발생하는 에러를 커스텀 에러로 변경하여 처리한다

--- src/main/java/spring/backend/core/exception/GlobalExceptionHandler.java
+++ src/main/java/spring/backend/core/exception/GlobalExceptionHandler.java
@@ -1,14 +1,18 @@
 package spring.backend.core.exception;
 
-import java.util.Optional;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import spring.backend.core.presentation.ErrorResponse;
+
+import java.util.Optional;
 
 @RestControllerAdvice
 @Log4j2
@@ -27,5 +31,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     log.error("ERROR ::: [DomainException] ", ex);
     ErrorResponse errorResponse = ErrorResponse.createDomainErrorResponse().statusCode(httpStatus.value()).exception(ex).build();
     return ResponseEntity.status(httpStatus).body(errorResponse);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+    log.error("ERROR ::: [MethodArgumentNotValidException] ", ex);
+    ErrorResponse errorResponse = ErrorResponse.createValidationErrorResponse().statusCode(400).exception(ex).build();
+    return ResponseEntity.badRequest().body(errorResponse);
   }
 }

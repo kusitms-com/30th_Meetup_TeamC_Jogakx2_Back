f3dea8b39cc235c00a145d59a8cddf2e067a283e
HoyeongJeon <hoyeong0302@gmail.com>
2024-10-22 14:48:47 +0900

    feat: (#35) rotateAccessToken 을 수정한다. - 쿠키에 refreshToken이 존재하지 않은 경우 예외를 발생시킨다. - 쿠키에 저장된 refreshToken과 저장소에 저장된 refreshToken이 일치하지 않는 경우 예외를 발생시킨다.

--- src/main/java/spring/backend/auth/application/RotateTokenService.java
+++ src/main/java/spring/backend/auth/application/RotateTokenService.java
@@ -1,10 +1,10 @@
 package spring.backend.auth.application;
 
-import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import spring.backend.auth.dto.response.RotateTokenResponse;
+import spring.backend.auth.exception.AuthenticationErrorCode;
 import spring.backend.core.application.JwtService;
 import spring.backend.member.application.MemberService;
 
@@ -16,9 +16,27 @@ import java.util.UUID;
 public class RotateTokenService {
     private final MemberService memberService;
     private final JwtService jwtService;
+    private final RefreshTokenService refreshTokenService;
 
     public RotateTokenResponse rotateAccessToken(String refreshToken) {
-        UUID memberId = UUID.fromString(jwtService.getPayload(refreshToken).get("memberId", String.class));
+        UUID memberId = extractMemberIdFromRefreshToken(refreshToken);
+        validateRefreshToken(memberId, refreshToken);
         return new RotateTokenResponse(jwtService.provideAccessToken(memberService.findByMemberId(memberId)));
+    }
+
+    private UUID extractMemberIdFromRefreshToken(String refreshToken) {
+        if(refreshToken == null) {
+            log.error("쿠키에 refreshToken이 존재하지 않습니다.");
+            throw AuthenticationErrorCode.MISSING_COOKIE_VALUE.toException();
+        }
+        return UUID.fromString(jwtService.getPayload(refreshToken).get("memberId", String.class));
+    }
+
+    private void validateRefreshToken(UUID memberId, String refreshToken) {
+        String savedRefreshToken = refreshTokenService.getRefreshToken(memberId);
+        if (!savedRefreshToken.equals(refreshToken)) {
+            log.error("리프레시 토큰이 저장소에 존재하지 않습니다.");
+            throw AuthenticationErrorCode.NOT_EXIST_REFRESH_TOKEN.toException();
+        }
     }
 }

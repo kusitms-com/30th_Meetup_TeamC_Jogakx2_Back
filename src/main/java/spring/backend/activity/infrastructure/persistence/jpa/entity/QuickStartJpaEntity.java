bdd3112dfd4e7b11532bf3a68bf06fb09d45185d
anxi01 <tjdals2893@naver.com>
2024-10-28 06:25:33 +0900

    feat: (#55) QuickStart Jpa 엔티티와 리포지터리를 생성한다

---
+++ src/main/java/spring/backend/activity/infrastructure/persistence/jpa/entity/QuickStartJpaEntity.java
@@ -0,0 +1,34 @@
+package spring.backend.activity.infrastructure.persistence.jpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import spring.backend.activity.domain.value.Type;
+import spring.backend.core.infrastructure.jpa.shared.BaseLongIdEntity;
+
+import java.sql.Time;
+import java.util.UUID;
+
+@Entity
+@Table(name = "quick_start")
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class QuickStartJpaEntity extends BaseLongIdEntity {
+
+    private UUID memberId;
+
+    private String name;
+
+    private Time startTime;
+
+    private Integer spareTime;
+
+    @Enumerated(EnumType.STRING)
+    private Type type;
+}

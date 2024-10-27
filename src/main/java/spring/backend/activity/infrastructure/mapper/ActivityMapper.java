52e6524d197dce256cb7ed91070a8a1f2f2f7e2c
anxi01 <tjdals2893@naver.com>
2024-10-28 06:26:35 +0900

    feat: (#55) Activity 매퍼 클래스와 리포지터리 어댑터를 추가한다

---
+++ src/main/java/spring/backend/activity/infrastructure/mapper/ActivityMapper.java
@@ -0,0 +1,51 @@
+package spring.backend.activity.infrastructure.mapper;
+
+import org.springframework.stereotype.Component;
+import spring.backend.activity.domain.entity.Activity;
+import spring.backend.activity.infrastructure.persistence.jpa.entity.ActivityJpaEntity;
+
+import java.util.Optional;
+
+@Component
+public class ActivityMapper {
+
+    public Activity toDomainEntity(ActivityJpaEntity activity) {
+        return Activity.builder()
+                .id(activity.getId())
+                .memberId(activity.getMemberId())
+                .quickStartId(activity.getQuickStartId())
+                .spareTime(activity.getSpareTime())
+                .type(activity.getType())
+                .keywords(activity.getKeywords())
+                .title(activity.getTitle())
+                .content(activity.getContent())
+                .location(activity.getLocation())
+                .finished(activity.getFinished())
+                .finishedAt(activity.getFinishedAt())
+                .savedTime(activity.getSavedTime())
+                .createdAt(activity.getCreatedAt())
+                .updatedAt(activity.getUpdatedAt())
+                .deleted(activity.getDeleted())
+                .build();
+    }
+
+    public ActivityJpaEntity toJpaEntity(Activity activity) {
+        return ActivityJpaEntity.builder()
+                .id(activity.getId())
+                .memberId(activity.getMemberId())
+                .quickStartId(activity.getQuickStartId())
+                .spareTime(activity.getSpareTime())
+                .type(activity.getType())
+                .keywords(activity.getKeywords())
+                .title(activity.getTitle())
+                .content(activity.getContent())
+                .location(activity.getLocation())
+                .finished(activity.getFinished())
+                .finishedAt(activity.getFinishedAt())
+                .savedTime(activity.getSavedTime())
+                .createdAt(activity.getCreatedAt())
+                .updatedAt(activity.getUpdatedAt())
+                .deleted(Optional.ofNullable(activity.getDeleted()).orElse(false))
+                .build();
+    }
+}

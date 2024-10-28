1930274287b79b1116d5c2126d46de6453262943
HoyeongJeon <hoyeong0302@gmail.com>
2024-10-28 22:35:32 +0900

    refactor: (#50) ClovaService 코드를 리팩토링한다. - ClovaService에서 설정했던 WebClient 헤더를 WebClientConfiguration 의 default 헤더로 변경한다. - ClovaService의 NULL 처리를 명확하게 변경하고, body를 bodyValue로 변경한다.(클로바 스튜디오로 요청을 보낼 시 request는 요청 전에 완성되어 있으므로 bodyValue로 변경함)

--- src/main/java/spring/backend/core/configuration/webclient/WebClientConfiguration.java
+++ src/main/java/spring/backend/core/configuration/webclient/WebClientConfiguration.java
@@ -1,14 +1,28 @@
 package spring.backend.core.configuration.webclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfiguration {
 
+    @Value("${clova.api.api-key}")
+    private String apiKey;
+
+    @Value("${clova.api.api-gateway-key}")
+    private String apiGatewayKey;
+
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
-        return builder.build();
+        return builder.defaultHeaders(
+                httpHeaders -> {
+                    httpHeaders.set("X-NCP-CLOVASTUDIO-API-KEY", apiKey);
+                    httpHeaders.set("X-NCP-APIGW-API-KEY", apiGatewayKey);
+                    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+                }
+        ).build();
     }
 }
--- src/main/java/spring/backend/recommendation/infrastructure/clova/application/ClovaService.java
+++ src/main/java/spring/backend/recommendation/infrastructure/clova/application/ClovaService.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
 import reactor.core.publisher.Mono;
 import spring.backend.core.exception.DomainException;
 import spring.backend.core.exception.error.GlobalErrorCode;
 import spring.backend.recommendation.infrastructure.clova.dto.request.ClovaRequest;
 import spring.backend.recommendation.dto.request.UserInputRequest;
 import spring.backend.recommendation.infrastructure.clova.dto.response.ClovaResponse;
+import spring.backend.recommendation.infrastructure.clova.exception.ClovaErrorCode;
 
 @Service
 @RequiredArgsConstructor
@@ -18,12 +20,6 @@ public class ClovaService {
 
     @Value("${clova.api.url}")
     private String apiUrl;
-
-    @Value("${clova.api.api-key}")
-    private String apiKey;
-
-    @Value("${clova.api.api-gateway-key}")
-    private String apiGatewayKey;
 
     private final WebClient webClient;
 
@@ -33,23 +29,21 @@ public class ClovaService {
 
             ClovaResponse result = webClient.post()
                     .uri(apiUrl)
-                    .headers(httpHeaders -> {
-                        httpHeaders.set("X-NCP-CLOVASTUDIO-API-KEY", apiKey);
-                        httpHeaders.set("X-NCP-APIGW-API-KEY", apiGatewayKey);
-                        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-                    })
-                    .body(Mono.just(request), ClovaRequest.class)
+                    .bodyValue(request)
                     .retrieve()
                     .bodyToMono(ClovaResponse.class)
                     .block();
 
-            assert result != null;
+            if (result == null || result.getResult() == null || result.getResult().getMessage() == null) {
+                throw ClovaErrorCode.NULL_RESPONSE_FROM_CLOVA.toException();
+            }
 
             return result.getResult().getMessage().getContent();
         } catch (DomainException e) {
             throw e;
-        }
-        catch (Exception e) {
+        } catch (WebClientException e) {
+            throw GlobalErrorCode.WEB_CLIENT_ERROR.toException();
+        } catch (Exception e) {
             throw GlobalErrorCode.INTERNAL_ERROR.toException();
         }
     }

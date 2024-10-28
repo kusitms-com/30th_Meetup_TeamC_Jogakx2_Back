5acdc8da9aa7015d2be82a062ec6c737af6b64b7
HoyeongJeon <hoyeong0302@gmail.com>
2024-10-28 16:48:42 +0900

    feat: (#50) 클로바 스튜디오 용 Request, Response DTO를 생성한다.

---
+++ src/main/java/spring/backend/recommendation/infrastructure/clova/dto/request/ClovaRequest.java
@@ -0,0 +1,37 @@
+package spring.backend.recommendation.infrastructure.clova.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import spring.backend.recommendation.dto.request.UserInputRequest;
+
+import java.util.ArrayList;
+
+@Builder
+@Getter
+public class ClovaRequest {
+    private ArrayList<Message> messages;
+    private double topP;
+    private int topK;
+    private int maxTokens;
+    private double temperature;
+    private double repeatPenalty;
+    private boolean includeAiFilters;
+    private int seed;
+
+    public static ClovaRequest createClovaRequest(UserInputRequest userInputRequest) {
+        ArrayList<Message> messages = new ArrayList<>();
+        messages.add(Message.createSystem());
+        messages.add(Message.createMessage(userInputRequest));
+
+        return ClovaRequest.builder()
+                .messages(messages)
+                .topP(0.8)
+                .topK(0)
+                .maxTokens(500)
+                .temperature(0.5)
+                .repeatPenalty(5.0)
+                .includeAiFilters(true)
+                .seed(0)
+                .build();
+    }
+}

e26430be3e414675a8faf5edc061462fc7fbf051
HoyeongJeon <hoyeong0302@gmail.com>
2024-10-28 16:47:47 +0900

    feat: (#50) 클로바 스튜디오에게 보낼 Message를 생성한다.

---
+++ src/main/java/spring/backend/recommendation/infrastructure/clova/dto/request/Message.java
@@ -0,0 +1,47 @@
+package spring.backend.recommendation.infrastructure.clova.dto.request;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import spring.backend.recommendation.dto.request.UserInputRequest;
+import spring.backend.recommendation.infrastructure.clova.exception.ClovaErrorCode;
+
+@Getter
+@Builder
+public class Message {
+
+    private ROLE role;
+    private String content;
+
+    public enum ROLE {
+        system, user
+    }
+
+    public static Message createSystem() {
+        String content = "너는 자투리시간과 활동의 타입(온라인,오프라인, 둘다)를 받고, 키워드와 위치 정보를 받아 사용자에게 5가지의 활동을 추천해주는 봇이야. 추천목록만 보내주면 되고, 각 추천목록은 줄바꿈으로 구분해줘. 추천목록을 제외한 잡설은 보내지마.";
+        return Message.builder()
+                .role(ROLE.system)
+                .content(content)
+                .build();
+    }
+
+    public static Message createMessage(UserInputRequest userInputRequest) {
+        return Message.builder().role(ROLE.user).content(createContent(userInputRequest)).build();
+    }
+
+    private static String createContent(UserInputRequest userInputRequest) {
+        int spareTime = userInputRequest.getSpareTime();
+        String activityType = userInputRequest.getActivityType().toString();
+        String keyword = userInputRequest.getKeyword();
+        String location = userInputRequest.getLocation();
+
+        if (activityType.equals("OFFLINE")) {
+            if (location == null) {
+                throw ClovaErrorCode.NOT_EXIST_LOCATION_WHEN_OFFLINE.toException();
+            }
+            return "자투리 시간: " + spareTime + "분\\n선호활동: " + activityType + "\\n활동 키워드: " + keyword + "\\n위치 : " + location + "\\n \\n활동 추천해줘\\n\\n";
+        } else {
+            return "자투리 시간: " + spareTime + "분\\n선호활동: " + activityType + "\\n활동 키워드: " + keyword + "\\n \\n활동 추천해줘\\n\\n";
+        }
+    }
+}

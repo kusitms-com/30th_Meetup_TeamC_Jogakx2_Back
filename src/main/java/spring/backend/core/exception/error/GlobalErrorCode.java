f84e8562dd5465c2ebbd2a5775d9ea45ba59ed03
HoyeongJeon <hoyeong0302@gmail.com>
2024-10-28 22:31:31 +0900

    feat: (#50) WebClient 에러와 Clova 관련 에러를 추가한다.

--- src/main/java/spring/backend/core/exception/error/GlobalErrorCode.java
+++ src/main/java/spring/backend/core/exception/error/GlobalErrorCode.java
@@ -10,7 +10,8 @@ public enum GlobalErrorCode implements BaseErrorCode<RuntimeException> {
 
   ALREADY_PROCESS_STARTED(HttpStatus.BAD_REQUEST, "이미 처리중인 요청입니다."),
   INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 내부 오류입니다."),
-  REDIS_CONNECTION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Redis 서버와의 연결에 문제가 발생했습니다.");
+  REDIS_CONNECTION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Redis 서버와의 연결에 문제가 발생했습니다."),
+  WEB_CLIENT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "WebClient 오류가 발생했습니다.");
   private final HttpStatus httpStatus;
 
   private final String message;
--- src/main/java/spring/backend/recommendation/infrastructure/clova/exception/ClovaErrorCode.java
+++ src/main/java/spring/backend/recommendation/infrastructure/clova/exception/ClovaErrorCode.java
@@ -12,7 +12,8 @@ import spring.backend.core.exception.error.BaseErrorCode;
 public enum ClovaErrorCode implements BaseErrorCode<DomainException> {
 
     NOT_EXIST_LOCATION_WHEN_OFFLINE(HttpStatus.BAD_REQUEST, "오프라인의 경우 위치 정보가 필수입니다."),
-    NO_RESPONSE_FROM_CLOVA(HttpStatus.INTERNAL_SERVER_ERROR, "클로바 서버로부터 응답이 없습니다.");
+    NO_RESPONSE_FROM_CLOVA(HttpStatus.INTERNAL_SERVER_ERROR, "클로바 서버로부터 응답이 없습니다."),
+    NULL_RESPONSE_FROM_CLOVA(HttpStatus.INTERNAL_SERVER_ERROR, "클로바 서버로부터 NULL값을 받았습니다.");
     private final HttpStatus httpStatus;
 
     private final String message;

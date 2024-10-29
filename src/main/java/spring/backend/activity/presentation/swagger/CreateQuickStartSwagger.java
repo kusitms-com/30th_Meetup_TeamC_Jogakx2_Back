048929544b9ee40cf9533ab11f3d6ad4f4b7a9c4
anxi01 <tjdals2893@naver.com>
2024-10-29 23:22:48 +0900

    feat: (#58) 빠른 시작 생성 API 스웨거를 작성한다

---
+++ src/main/java/spring/backend/activity/presentation/swagger/CreateQuickStartSwagger.java
@@ -0,0 +1,24 @@
+package spring.backend.activity.presentation.swagger;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import spring.backend.activity.dto.request.QuickStartRequest;
+import spring.backend.activity.exception.QuickStartErrorCode;
+import spring.backend.core.configuration.swagger.ApiErrorCode;
+import spring.backend.core.exception.error.GlobalErrorCode;
+import spring.backend.core.presentation.RestResponse;
+import spring.backend.member.domain.entity.Member;
+
+@Tag(name = "QuickStart", description = "빠른 시작")
+public interface CreateQuickStartSwagger {
+
+    @Operation(
+            summary = "빠른 시작 생성 API",
+            description = "빠른 시작을 생성합니다.",
+            operationId = "/v1/quick-starts"
+    )
+    @ApiErrorCode({GlobalErrorCode.class, QuickStartErrorCode.class})
+    ResponseEntity<RestResponse<Long>> createQuickStart(@Parameter(hidden = true) Member member, QuickStartRequest request);
+}

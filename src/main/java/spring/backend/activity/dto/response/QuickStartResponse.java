9f3564bfa7bdf448388bcac2930fbbdf5a33f23e
anxi01 <tjdals2893@naver.com>
2024-10-30 03:02:23 +0900

    feat: (#60) 빠른 시작 리스트 조회 스웨거를 작성한다

--- src/main/java/spring/backend/activity/dto/response/QuickStartResponse.java
+++ src/main/java/spring/backend/activity/dto/response/QuickStartResponse.java
@@ -1,14 +1,25 @@
 package spring.backend.activity.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import spring.backend.activity.domain.value.Type;
 
 import java.sql.Time;
 
 public record QuickStartResponse(
+
+        @Schema(description = "빠른 시작 ID", example = "1")
         Long id,
+
+        @Schema(description = "빠른 시작 이름", example = "등교")
         String name,
+
+        @Schema(description = "시작 시간", example = "12:30:00")
         Time startTime,
+
+        @Schema(description = "자투리 시간", example = "300")
         Integer spareTime,
+
+        @Schema(description = "활동 유형 (ONLINE, OFFLINE, ONLINE_AND_OFFLINE)", example = "ONLINE")
         Type type
 ) {
 }
--- src/main/java/spring/backend/activity/dto/response/QuickStartsResponse.java
+++ src/main/java/spring/backend/activity/dto/response/QuickStartsResponse.java
@@ -1,8 +1,12 @@
 package spring.backend.activity.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
 public record QuickStartsResponse(
+
+        @Schema(description = "빠른 시작 리스트")
         List<QuickStartResponse> quickStartResponses
 ) {
 }

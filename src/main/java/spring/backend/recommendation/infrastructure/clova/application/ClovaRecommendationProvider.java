7ecd04647d35b845e94f30657b001801b218f48c
HoyeongJeon <hoyeong0302@gmail.com>
2024-10-30 13:14:39 +0900

    refactor: (#50) 추천 도메인의 구조를 DIP와 DDD 원칙에 맞게 개선한다.

---
+++ src/main/java/spring/backend/recommendation/infrastructure/clova/application/ClovaRecommendationProvider.java
@@ -0,0 +1,29 @@
+package spring.backend.recommendation.infrastructure.clova.application;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import spring.backend.recommendation.dto.request.ClovaRecommendationRequest;
+import spring.backend.recommendation.application.RecommendationProvider;
+import spring.backend.recommendation.infrastructure.clova.dto.response.ClovaResponse;
+import spring.backend.recommendation.infrastructure.clova.exception.ClovaErrorCode;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class ClovaRecommendationProvider implements RecommendationProvider {
+
+    private final ClovaService clovaService;
+
+    @Override
+    public String requestToClovaStudio(ClovaRecommendationRequest clovaRecommendationRequest) {
+        ClovaResponse result = clovaService.requestToClovaStudio(clovaRecommendationRequest);
+
+        if (result == null || result.getResult() == null || result.getResult().getMessage() == null || result.getResult().getMessage().getContent() == null) {
+            log.error("Clova 서비스로부터 null 응답을 수신했습니다. 요청 내용: {}", clovaRecommendationRequest);
+            throw ClovaErrorCode.NULL_RESPONSE_FROM_CLOVA.toException();
+        }
+
+        return result.getResult().getMessage().getContent();
+    }
+}

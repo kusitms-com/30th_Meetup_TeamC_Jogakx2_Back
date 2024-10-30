a555629e90490aa572f3d5c5f16868eca232c298
HoyeongJeon <hoyeong0302@gmail.com>
2024-10-30 12:33:31 +0900

    refactor: (#50) Clova에서 발생하는 에러를 비즈니스 로직에서 처리하도록 변경한다.

--- src/main/java/spring/backend/recommendation/infrastructure/clova/application/ClovaService.java
+++ src/main/java/spring/backend/recommendation/infrastructure/clova/application/ClovaService.java
@@ -1,47 +1,44 @@
 package spring.backend.recommendation.infrastructure.clova.application;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientException;
 import spring.backend.core.exception.DomainException;
 import spring.backend.core.exception.error.GlobalErrorCode;
-import spring.backend.recommendation.infrastructure.clova.dto.request.ClovaRequest;
 import spring.backend.recommendation.dto.request.ClovaRecommendationRequest;
+import spring.backend.recommendation.infrastructure.clova.dto.request.ClovaRequest;
 import spring.backend.recommendation.infrastructure.clova.dto.response.ClovaResponse;
 import spring.backend.recommendation.infrastructure.clova.exception.ClovaErrorCode;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class ClovaService {
 
     @Value("${clova.api.url}")
     private String apiUrl;
 
-    private final WebClient webClient;
+    private final WebClient clovaStudioWebClient;
 
-    public String requestToClovaStudio(ClovaRecommendationRequest clovaRecommendationRequest) {
+    public ClovaResponse requestToClovaStudio(ClovaRecommendationRequest clovaRecommendationRequest) {
         try {
             ClovaRequest request = ClovaRequest.createClovaRequest(clovaRecommendationRequest);
 
-            ClovaResponse result = webClient.post()
+            return clovaStudioWebClient.post()
                     .uri(apiUrl)
                     .bodyValue(request)
                     .retrieve()
                     .bodyToMono(ClovaResponse.class)
                     .block();
-
-            if (result == null || result.getResult() == null || result.getResult().getMessage() == null) {
-                throw ClovaErrorCode.NULL_RESPONSE_FROM_CLOVA.toException();
-            }
-
-            return result.getResult().getMessage().getContent();
-        } catch (DomainException e) {
-            throw e;
         } catch (WebClientException e) {
+            log.error("WebClient 에러 발생 - 에러 메시지: {}", e.getMessage(), e);
             throw GlobalErrorCode.WEB_CLIENT_ERROR.toException();
         } catch (Exception e) {
+            log.error("알 수 없는 내부 오류 발생 - 에러 메시지: {}", e.getMessage(), e);
             throw GlobalErrorCode.INTERNAL_ERROR.toException();
         }
     }

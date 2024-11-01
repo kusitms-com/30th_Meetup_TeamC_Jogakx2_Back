336f7c1a0bf5ae2d43ee5fc6fc02247860a9b8c8
anxi01 <tjdals2893@naver.com>
2024-11-02 02:34:17 +0900

    feat: (#70) 홈 메인 조회 API 스웨거를 작성한다

--- src/main/java/spring/backend/activity/dto/response/HomeActivityInfoResponse.java
+++ src/main/java/spring/backend/activity/dto/response/HomeActivityInfoResponse.java
@@ -1,11 +1,20 @@
 package spring.backend.activity.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import spring.backend.activity.domain.value.Keyword;
 
 public record HomeActivityInfoResponse(
+
+        @Schema(description = "활동 ID", example = "1")
         Long id,
+
+        @Schema(description = "활동 키워드", example = "{\"category\": \"SELF_DEVELOPMENT\", \"image\": \"https://example.com/image.jpg\"}")
         Keyword keyword,
+
+        @Schema(description = "활동 제목", example = "마음의 편안을 가져다주는 명상음악 20분 듣기")
         String title,
+
+        @Schema(description = "모은 시간", example = "60")
         Integer savedTime
 ) {
 }
---
+++ src/main/java/spring/backend/member/presentation/swagger/ReadMemberHomeSwagger.java
@@ -0,0 +1,24 @@
+package spring.backend.member.presentation.swagger;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import spring.backend.core.configuration.swagger.ApiErrorCode;
+import spring.backend.core.exception.error.GlobalErrorCode;
+import spring.backend.core.presentation.RestResponse;
+import spring.backend.member.domain.entity.Member;
+import spring.backend.member.dto.response.HomeMainResponse;
+import spring.backend.member.exception.MemberErrorCode;
+
+@Tag(name = "Member", description = "멤버")
+public interface ReadMemberHomeSwagger {
+
+    @Operation(
+            summary = "홈 메인페이지 조회 API",
+            description = "사용자의 가장 근접한 빠른시작과 당일 모은 활동내역을 보여줍니다.",
+            operationId = "/v1/home"
+    )
+    @ApiErrorCode({GlobalErrorCode.class, MemberErrorCode.class})
+    ResponseEntity<RestResponse<HomeMainResponse>> readMemberHome(@Parameter(hidden = true) Member member);
+}

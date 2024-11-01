496850b8431ee3a2eb01997eb632e5994f5c40ce
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-01 11:13:26 +0900

    feat: (#64) 유저 활동 선택 서비스를 만든다.

---
+++ src/main/java/spring/backend/activity/application/UserActivitySelectService.java
@@ -0,0 +1,34 @@
+package spring.backend.activity.application;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import spring.backend.activity.domain.entity.Activity;
+import spring.backend.activity.domain.repository.ActivityRepository;
+import spring.backend.activity.dto.request.UserActivitySelectRequest;
+import spring.backend.activity.exception.ActivityErrorCode;
+import spring.backend.member.domain.entity.Member;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+@Transactional
+public class UserActivitySelectService {
+
+    private final ActivityRepository activityRepository;
+
+    public Long userActivitySelection(Member member, UserActivitySelectRequest userActivitySelectRequest) {
+        validateRequest(userActivitySelectRequest);
+        Activity activity = Activity.create(member.getId(), userActivitySelectRequest.spareTime(), userActivitySelectRequest.type(), userActivitySelectRequest.keywords(), userActivitySelectRequest.title(), userActivitySelectRequest.content(), userActivitySelectRequest.location());
+        Activity savedActivity = activityRepository.save(activity);
+        return savedActivity.getId();
+    }
+
+    private void validateRequest(UserActivitySelectRequest userActivitySelectRequest) {
+        if (userActivitySelectRequest == null) {
+            log.error("[UserActivitySelectRequest] Invalid request.");
+            throw ActivityErrorCode.NOT_EXIST_ACTIVITY_CONDITION.toException();
+        }
+    }
+}

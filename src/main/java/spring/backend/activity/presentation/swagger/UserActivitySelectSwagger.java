eb887cca6bfe23ce8b6e35d66107ef656ed54500
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-01 11:12:34 +0900

    feat: (#64) 사용자 활동 선택 스웨거를 생성하고 컨트롤러를 작성한다.

---
+++ src/main/java/spring/backend/activity/presentation/swagger/UserActivitySelectSwagger.java
@@ -0,0 +1,24 @@
+package spring.backend.activity.presentation.swagger;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import spring.backend.activity.dto.request.UserActivitySelectRequest;
+import spring.backend.activity.exception.ActivityErrorCode;
+import spring.backend.core.configuration.swagger.ApiErrorCode;
+import spring.backend.core.exception.error.GlobalErrorCode;
+import spring.backend.core.presentation.RestResponse;
+import spring.backend.member.domain.entity.Member;
+
+@Tag(name = "UserActivitySelection", description = "활동 선택")
+public interface UserActivitySelectSwagger {
+
+    @Operation(
+            summary = "사용자 활동 선택 API",
+            description = "사용자가 추천받은 활동 중 한가지 활동을 선택합니다.",
+            operationId = "/v1/user-activity-selection"
+    )
+    @ApiErrorCode({GlobalErrorCode.class, ActivityErrorCode.class})
+    ResponseEntity<RestResponse<Long>>  userActivitySelection(@Parameter(hidden = true) Member member, UserActivitySelectRequest userActivitySelectRequest);
+}

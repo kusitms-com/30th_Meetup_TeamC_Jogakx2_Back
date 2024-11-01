2640dc86c7a3bed1cd18fc670aac658f19e1261f
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-02 00:02:49 +0900

    fix: (#64) 사용자가 선택한 활동 키워드는 한 개만 존재한다.

--- src/main/java/spring/backend/activity/dto/request/UserActivitySelectRequest.java
+++ src/main/java/spring/backend/activity/dto/request/UserActivitySelectRequest.java
@@ -20,10 +20,10 @@ public record UserActivitySelectRequest(
         @Schema(description = "자투리 시간", example = "300")
         Integer spareTime,
 
-        @NotNull(message = "키워드는 최대 5가지 입력 가능하며, 키워드를 선택하지 않은 경우 빈 배열을 보내주세요")
+        @NotNull(message = "키워드는 필수 입력 항목입니다.")
         @Schema(description = "키워드(SELF_DEVELOPMENT, HEALTH, NATURE, CULTURE_ART, ENTERTAINMENT, RELAXATION, SOCIAL)",
-                example = "[{\"category\": \"SELF_DEVELOPMENT\", \"image\": \"image_url\"}, {\"category\": \"HEALTH\", \"image\": \"image_url\"}]")
-        Set<Keyword> keywords,
+                example = "[{\"category\": \"SELF_DEVELOPMENT\", \"image\": \"image_url\"}]")
+        Keyword keyword,
 
         @NotNull(message = "타이틀은 필수 입력 항목입니다.")
         @Schema(description = "타이틀", example = "카페에서 커피 마시며 책 읽기")
--- src/main/java/spring/backend/activity/exception/ActivityErrorCode.java
+++ src/main/java/spring/backend/activity/exception/ActivityErrorCode.java
@@ -14,7 +14,8 @@ public enum ActivityErrorCode implements BaseErrorCode<DomainException> {
     NOT_EXIST_ACTIVITY(HttpStatus.BAD_REQUEST, "활동이 존재하지 않습니다."),
     MEMBER_ID_MISMATCH(HttpStatus.FORBIDDEN, "활동과 멤버 ID가 일치하지 않습니다."),
     INVALID_ACTIVITY_DURATION(HttpStatus.BAD_REQUEST, "활동 지속 시간이 허용된 범위를 초과했습니다."),
-    ALREADY_FINISHED_ACTIVITY(HttpStatus.BAD_REQUEST, "이미 종료된 활동입니다.");
+    ALREADY_FINISHED_ACTIVITY(HttpStatus.BAD_REQUEST, "이미 종료된 활동입니다."),
+    NOT_EXIST_ACTIVITY_CONDITION(HttpStatus.BAD_REQUEST, "요청이 비어있습니다.");
 
     private final HttpStatus httpStatus;
 

62a0bed0d68d71f644750fcb42db0457e88d3cb3
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-02 00:02:15 +0900

    feat: (#64) ClovaStudio의 응답에 Keyword를 추가한다.

--- src/main/java/spring/backend/recommendation/dto/response/ClovaRecommendationResponse.java
+++ src/main/java/spring/backend/recommendation/dto/response/ClovaRecommendationResponse.java
@@ -2,6 +2,7 @@ package spring.backend.recommendation.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import spring.backend.activity.domain.value.Keyword;
 
 @Getter
 @AllArgsConstructor
@@ -9,4 +10,5 @@ public class ClovaRecommendationResponse {
     private Integer order;
     private String title;
     private String content;
+    private Keyword.Category keywordCategory;
 }

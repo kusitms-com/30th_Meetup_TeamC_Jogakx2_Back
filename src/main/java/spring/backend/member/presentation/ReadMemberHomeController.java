e5f1aeeb3ce74a97e6af5283d93b03121447e3ea
anxi01 <tjdals2893@naver.com>
2024-11-02 02:06:52 +0900

    feat: (#70) 사용자가 홈 화면을 조회할 수 있다

---
+++ src/main/java/spring/backend/member/presentation/ReadMemberHomeController.java
@@ -0,0 +1,25 @@
+package spring.backend.member.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spring.backend.core.configuration.argumentresolver.AuthorizedMember;
+import spring.backend.core.configuration.interceptor.Authorization;
+import spring.backend.core.presentation.RestResponse;
+import spring.backend.member.application.ReadMemberHomeService;
+import spring.backend.member.domain.entity.Member;
+import spring.backend.member.dto.response.HomeMainResponse;
+
+@RestController
+@RequiredArgsConstructor
+public class ReadMemberHomeController {
+
+    private final ReadMemberHomeService readMemberHomeService;
+
+    @Authorization
+    @GetMapping("/v1/home")
+    public ResponseEntity<RestResponse<HomeMainResponse>> readMemberHome(@AuthorizedMember Member member) {
+        return ResponseEntity.ok(new RestResponse<>(readMemberHomeService.readMemberHome(member)));
+    }
+}

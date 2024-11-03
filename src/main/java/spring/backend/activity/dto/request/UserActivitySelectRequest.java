ccdc446430f1607ebebe8c63a23b1d2613bd106d
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-03 18:54:40 +0900

    fix: (#64) Error 메세지를 한글로 통일한다.

--- src/main/java/spring/backend/activity/dto/request/UserActivitySelectRequest.java
+++ src/main/java/spring/backend/activity/dto/request/UserActivitySelectRequest.java
@@ -21,15 +21,14 @@ public record UserActivitySelectRequest(
         Integer spareTime,
 
         @NotNull(message = "키워드는 필수 입력 항목입니다.")
-        @Schema(description = "키워드(SELF_DEVELOPMENT, HEALTH, NATURE, CULTURE_ART, ENTERTAINMENT, RELAXATION, SOCIAL)",
-                example = "[{\"category\": \"SELF_DEVELOPMENT\", \"image\": \"image_url\"}]")
+        @Schema(description = "활동 키워드", example = "{\"category\": \"SELF_DEVELOPMENT\", \"image\": \"https://example.com/image.jpg\"}")
         Keyword keyword,
 
         @NotNull(message = "타이틀은 필수 입력 항목입니다.")
         @Schema(description = "타이틀", example = "카페에서 커피 마시며 책 읽기")
         String title,
 
-        @NotNull(message = "Content는 필수 입력 항목입니다.")
+        @NotNull(message = "내용은 필수 입력 항목입니다.")
         @Schema(description = "내용", example = "조용한 카페에서 좋아하는 책을 읽으며 여유로운 시간을 즐길 수 있습니다.")
         String content,
 

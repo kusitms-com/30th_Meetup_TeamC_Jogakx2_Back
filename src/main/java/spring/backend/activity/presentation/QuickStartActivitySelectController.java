b8940227274f2f42c7fef5925f0a374ba0dd0086
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-03 21:43:14 +0900

    feat: (#65) QuickStartActivitySelectSwagger를 만들고 Controller를 구현한다.

--- src/main/java/spring/backend/activity/presentation/QuickStartActivitySelectController.java
+++ src/main/java/spring/backend/activity/presentation/QuickStartActivitySelectController.java
@@ -1,14 +1,19 @@
 package spring.backend.activity.presentation;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import spring.backend.activity.application.QuickStartActivitySelectService;
 import spring.backend.activity.dto.request.QuickStartActivitySelectRequest;
 import spring.backend.activity.presentation.swagger.QuickStartActivitySelectSwagger;
+import spring.backend.core.configuration.argumentresolver.AuthorizedMember;
 import spring.backend.core.configuration.argumentresolver.LoginMember;
 import spring.backend.core.configuration.interceptor.Authorization;
+import spring.backend.core.presentation.RestResponse;
 import spring.backend.member.domain.entity.Member;
 
 @RestController
@@ -20,9 +25,8 @@ public class QuickStartActivitySelectController implements QuickStartActivitySel
     @Override
     @Authorization
     @PostMapping("/v1/quick-starts/{quickStartId}/activities")
-    public Long quickStartUserActivitySelect(@LoginMember Member member, @PathVariable Long quickStartId, QuickStartActivitySelectRequest quickStartActivitySelectRequest) {
-        // TODO Auto-generated method stub
-        Long result = quickStartActivitySelectService.quickStartUserActivitySelect(member, quickStartId, quickStartActivitySelectRequest);
-        return result;
+    public ResponseEntity<RestResponse<Long>> quickStartUserActivitySelect(@AuthorizedMember Member member, @PathVariable Long quickStartId, @Valid @RequestBody QuickStartActivitySelectRequest quickStartActivitySelectRequest) {
+        Long savedActivityIdCreatedByQuickStart = quickStartActivitySelectService.quickStartUserActivitySelect(member, quickStartId, quickStartActivitySelectRequest);
+        return ResponseEntity.ok(new RestResponse<>(savedActivityIdCreatedByQuickStart));
     }
 }

7bf60d4ece1b82ae76039401963b276be89c678a
anxi01 <tjdals2893@naver.com>
2024-11-04 18:17:31 +0900

    feat: (#77) 활동 종료 API 스웨거를 작성한다

--- src/main/java/spring/backend/activity/presentation/FinishActivityController.java
+++ src/main/java/spring/backend/activity/presentation/FinishActivityController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import spring.backend.activity.application.FinishActivityService;
 import spring.backend.activity.dto.response.FinishActivityResponse;
+import spring.backend.activity.presentation.swagger.FinishActivitySwagger;
 import spring.backend.core.configuration.argumentresolver.AuthorizedMember;
 import spring.backend.core.configuration.interceptor.Authorization;
 import spring.backend.core.presentation.RestResponse;
@@ -14,12 +15,12 @@ import spring.backend.member.domain.entity.Member;
 
 @RestController
 @RequiredArgsConstructor
-public class FinishActivityController {
+public class FinishActivityController implements FinishActivitySwagger {
 
     private final FinishActivityService finishActivityService;
 
     @Authorization
-    @PatchMapping("/v1/activities/{activityId}/end")
+    @PatchMapping("/v1/activities/{activityId}/finish")
     public ResponseEntity<RestResponse<FinishActivityResponse>> finishActivity(@AuthorizedMember Member member, @PathVariable Long activityId) {
         return ResponseEntity.ok(new RestResponse<>(finishActivityService.finishActivity(member, activityId)));
     }

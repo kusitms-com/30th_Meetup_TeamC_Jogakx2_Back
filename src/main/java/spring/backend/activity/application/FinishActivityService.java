245dd26e3beb4a8126bea8604cb67042d3f74afc
anxi01 <tjdals2893@naver.com>
2024-11-04 18:10:16 +0900

    feat: (#77) 사용자가 활동을 종료할 수 있다

---
+++ src/main/java/spring/backend/activity/application/FinishActivityService.java
@@ -0,0 +1,44 @@
+package spring.backend.activity.application;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import spring.backend.activity.domain.entity.Activity;
+import spring.backend.activity.domain.repository.ActivityRepository;
+import spring.backend.activity.dto.response.ActivityInfo;
+import spring.backend.activity.dto.response.FinishActivityResponse;
+import spring.backend.activity.exception.ActivityErrorCode;
+import spring.backend.member.domain.entity.Member;
+import spring.backend.member.dto.response.HomeMemberInfoResponse;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+@Transactional
+public class FinishActivityService {
+
+    private final ActivityRepository activityRepository;
+
+    public FinishActivityResponse finishActivity(Member member, Long activityId) {
+        Activity activity = activityRepository.findById(activityId);
+        validateActivity(activity, member);
+        activity.finish();
+        activityRepository.save(activity);
+        return toResponse(member, activity);
+    }
+
+    private void validateActivity(Activity activity, Member member) {
+        if (activity == null) {
+            log.error("[FinishActivityService.validateActivity] activity is null");
+            throw ActivityErrorCode.NOT_EXIST_ACTIVITY.toException();
+        }
+        activity.validateActivityOwner(member.getId());
+    }
+
+    private FinishActivityResponse toResponse(Member member, Activity activity) {
+        HomeMemberInfoResponse memberInfo = HomeMemberInfoResponse.from(member);
+        ActivityInfo activityInfo = ActivityInfo.from(activity);
+        return new FinishActivityResponse(memberInfo, activityInfo);
+    }
+}
---
+++ src/main/java/spring/backend/activity/presentation/FinishActivityController.java
@@ -0,0 +1,26 @@
+package spring.backend.activity.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import spring.backend.activity.application.FinishActivityService;
+import spring.backend.activity.dto.response.FinishActivityResponse;
+import spring.backend.core.configuration.argumentresolver.AuthorizedMember;
+import spring.backend.core.configuration.interceptor.Authorization;
+import spring.backend.core.presentation.RestResponse;
+import spring.backend.member.domain.entity.Member;
+
+@RestController
+@RequiredArgsConstructor
+public class FinishActivityController {
+
+    private final FinishActivityService finishActivityService;
+
+    @Authorization
+    @PatchMapping("/v1/activities/{activityId}/end")
+    public ResponseEntity<RestResponse<FinishActivityResponse>> finishActivity(@AuthorizedMember Member member, @PathVariable Long activityId) {
+        return ResponseEntity.ok(new RestResponse<>(finishActivityService.finishActivity(member, activityId)));
+    }
+}

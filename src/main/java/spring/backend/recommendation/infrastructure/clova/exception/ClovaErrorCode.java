0ea7b233f7230cce9f1a88f24ce44e32cda5d2bb
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-08 01:36:47 +0900

    feat: (#64) 추천활동 중 키워드가 올바르지 않은 경우 에러코드를 추가한다.

--- src/main/java/spring/backend/recommendation/infrastructure/clova/exception/ClovaErrorCode.java
+++ src/main/java/spring/backend/recommendation/infrastructure/clova/exception/ClovaErrorCode.java
@@ -15,7 +15,9 @@ public enum ClovaErrorCode implements BaseErrorCode<DomainException> {
     NO_RESPONSE_FROM_CLOVA(HttpStatus.INTERNAL_SERVER_ERROR, "클로바 서버로부터 응답이 없습니다."),
     NULL_RESPONSE_FROM_CLOVA(HttpStatus.INTERNAL_SERVER_ERROR, "클로바 서버로부터 NULL값을 받았습니다."),
     ONLINE_TYPE_CONTAIN_NATURE(HttpStatus.BAD_REQUEST, "선호하는 활동 타입이 ONLINE인 경우, NATURE(자연) 키워드를 사용할 수 없습니다."),
-    OFFLINE_TYPE_CONTAIN_SOCIAL(HttpStatus.BAD_REQUEST, "선호하는 활동 타입이 OFFLINE인 경우, SOCIAL(소셜) 키워드를 사용할 수 없습니다.");
+    OFFLINE_TYPE_CONTAIN_SOCIAL(HttpStatus.BAD_REQUEST, "선호하는 활동 타입이 OFFLINE인 경우, SOCIAL(소셜) 키워드를 사용할 수 없습니다."),
+    INVALID_KEYWORD_IN_RECOMMENDATIONS(HttpStatus.BAD_REQUEST, "추천 활동의 키워드가 올바르지 않습니다.");
+
     private final HttpStatus httpStatus;
 
     private final String message;

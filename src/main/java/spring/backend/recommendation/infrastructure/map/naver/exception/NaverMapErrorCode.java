e696eac42993da38fe4891df6a40e4be24330ddf
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-11 16:01:56 +0900

    feat: (#80) NaverMapErrorCode에 파싱 에러를 추가한다.

--- src/main/java/spring/backend/recommendation/infrastructure/map/naver/exception/NaverMapErrorCode.java
+++ src/main/java/spring/backend/recommendation/infrastructure/map/naver/exception/NaverMapErrorCode.java
@@ -11,7 +11,8 @@ import spring.backend.core.exception.error.BaseErrorCode;
 public enum NaverMapErrorCode implements BaseErrorCode<DomainException> {
     API_REQUEST_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "API 요청과 응답이 실패했습니다."),
     FAILED_TO_CONNECT_API(HttpStatus.INTERNAL_SERVER_ERROR, "API 연결에 실패했습니다."),
-    FAILED_TO_READ_RESPONSE(HttpStatus.INTERNAL_SERVER_ERROR, "API 응답을 읽는데 실패했습니다.");
+    FAILED_TO_READ_RESPONSE(HttpStatus.INTERNAL_SERVER_ERROR, "API 응답을 읽는데 실패했습니다."),
+    FAILED_TO_PARSE_RESPONSE(HttpStatus.INTERNAL_SERVER_ERROR, "응답을 파싱하는데 실패했습니다.");
 
     private final HttpStatus httpStatus;
 

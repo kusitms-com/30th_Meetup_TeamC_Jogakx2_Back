6876c30009fc550524c3fb1500ec7eab85884f4d
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-11 16:02:27 +0900

    feat: (#80) NaverMapResponse로 반환값을 변경한다.

--- src/main/java/spring/backend/recommendation/application/MapService.java
+++ src/main/java/spring/backend/recommendation/application/MapService.java
@@ -1,5 +1,7 @@
 package spring.backend.recommendation.application;
 
+import spring.backend.recommendation.infrastructure.map.naver.dto.response.NaverMapResponse;
+
 public interface MapService {
-    void search(String query);
+    NaverMapResponse search(String query);
 }

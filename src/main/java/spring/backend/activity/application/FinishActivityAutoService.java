bd0ce5ad05e6bc06e193292f1e645ef3c9140fc7
anxi01 <tjdals2893@naver.com>
2024-11-15 08:57:38 +0900

    feat: (#78) 활동의 자투리시간으로 종료 큐 메시지를 발행한다

---
+++ src/main/java/spring/backend/activity/application/FinishActivityAutoService.java
@@ -0,0 +1,28 @@
+package spring.backend.activity.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import spring.backend.activity.domain.entity.Activity;
+import spring.backend.activity.infrastructure.queue.FinishActivityMessage;
+import spring.backend.activity.infrastructure.queue.FinishActivityMessageProducer;
+import spring.backend.core.configuration.property.queue.FinishActivityQueueProperty;
+
+@Service
+@RequiredArgsConstructor
+public class FinishActivityAutoService {
+
+    private final FinishActivityQueueProperty finishActivityQueueProperty;
+
+    public void finishActivityAuto(Activity activity) {
+        int spareTime = activity.getSpareTime();
+        FinishActivityMessageProducer finishActivityMessageProducer = new FinishActivityMessageProducer(
+                finishActivityQueueProperty,
+                FinishActivityMessage.builder()
+                        .activityId(activity.getId())
+                        .spareTime(spareTime)
+                        .build()
+        );
+        long delayTime = (long) spareTime * 60 * 1000;
+        finishActivityMessageProducer.publishMessageWithDelay(delayTime);
+    }
+}

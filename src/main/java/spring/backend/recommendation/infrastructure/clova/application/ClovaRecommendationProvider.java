2c6c97b5cb4369c75cada4f8940de9b6ecf9fb21
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-14 13:00:45 +0900

    refactor: (#91) WebClient를 Provider에서 직접 구현한다.

--- src/main/java/spring/backend/recommendation/infrastructure/clova/application/ClovaRecommendationProvider.java
+++ src/main/java/spring/backend/recommendation/infrastructure/clova/application/ClovaRecommendationProvider.java
@@ -3,6 +3,7 @@ package spring.backend.recommendation.infrastructure.clova.application;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientException;
@@ -11,7 +12,6 @@ import spring.backend.recommendation.application.RecommendationProvider;
 import spring.backend.recommendation.dto.request.AIRecommendationRequest;
 import spring.backend.recommendation.infrastructure.clova.dto.request.ClovaRequest;
 import spring.backend.recommendation.infrastructure.clova.dto.response.ClovaResponse;
-import spring.backend.recommendation.infrastructure.clova.exception.ClovaErrorCode;
 
 @Component
 @RequiredArgsConstructor
@@ -21,14 +21,27 @@ public class ClovaRecommendationProvider implements RecommendationProvider<Clova
     @Value("${clova.api.url}")
     private String apiUrl;
 
-    private final WebClient clovaStudioWebClient;
+    @Value("${clova.api.api-key}")
+    private String apiKey;
+
+    @Value("${clova.api.api-gateway-key}")
+    private String apiGatewayKey;
 
     @Override
     public ClovaResponse getRecommendations(AIRecommendationRequest aiRecommendationRequest) {
         try {
             ClovaRequest request = ClovaRequest.createClovaRequest(aiRecommendationRequest);
 
-            return clovaStudioWebClient.post()
+            WebClient webClient = WebClient.builder()
+                    .defaultHeaders(httpHeaders -> {
+                        httpHeaders.set("X-NCP-CLOVASTUDIO-API-KEY", apiKey);
+                        httpHeaders.set("X-NCP-APIGW-API-KEY", apiGatewayKey);
+                        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+                    })
+                    .build();
+
+            return webClient
+                    .post()
                     .uri(apiUrl)
                     .bodyValue(request)
                     .retrieve()
@@ -41,12 +54,5 @@ public class ClovaRecommendationProvider implements RecommendationProvider<Clova
             log.error("알 수 없는 내부 오류 발생 - 에러 메시지: {}", e.getMessage(), e);
             throw GlobalErrorCode.INTERNAL_ERROR.toException();
         }
-
-//        if (result == null || result.getResult() == null || result.getResult().getMessage() == null || result.getResult().getMessage().getContent() == null) {
-//            log.error("Clova 서비스로부터 null 응답을 수신했습니다. 요청 내용: {}", clovaRecommendationRequest);
-//            throw ClovaErrorCode.NULL_RESPONSE_FROM_CLOVA.toException();
-//        }
-//
-//        return result;
     }
 }

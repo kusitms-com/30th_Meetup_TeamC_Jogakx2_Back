90b432d66851f72d1fa75356c41c5f108aa5b118
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-15 17:33:37 +0900

    feat: (#87) 월간 활동 요약을 제공하는 컨트롤러를 구현한다.

---
+++ src/main/java/spring/backend/activity/presentation/ReadMonthlyActivityOverviewController.java
@@ -0,0 +1,32 @@
+package spring.backend.activity.presentation;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
+import spring.backend.activity.application.ReadMonthlyActivityOverviewService;
+import spring.backend.activity.dto.request.MonthlyActivityOverviewRequest;
+import spring.backend.activity.dto.response.MonthlyActivityOverviewResponse;
+import spring.backend.activity.presentation.swagger.ReadMonthlyActivityOverviewSwagger;
+import spring.backend.core.configuration.argumentresolver.AuthorizedMember;
+import spring.backend.core.configuration.interceptor.Authorization;
+import spring.backend.core.presentation.RestResponse;
+import spring.backend.member.domain.entity.Member;
+
+@RestController
+@RequiredArgsConstructor
+public class ReadMonthlyActivityOverviewController implements ReadMonthlyActivityOverviewSwagger {
+    private final ReadMonthlyActivityOverviewService readMonthlyActivityOverviewService;
+
+    @Authorization
+    @GetMapping("/v1/activity")
+    public ResponseEntity<RestResponse<MonthlyActivityOverviewResponse>> readMonthlyActivityOverviewController(
+            @AuthorizedMember Member member,
+            @Valid @ModelAttribute MonthlyActivityOverviewRequest monthlyActivityOverviewRequest
+    ) {
+        MonthlyActivityOverviewResponse monthlyActivityOverviewResponse = readMonthlyActivityOverviewService.readMonthlyActivityOverview(member, monthlyActivityOverviewRequest);
+        return ResponseEntity.ok(new RestResponse<>(monthlyActivityOverviewResponse));
+    }
+}
---
+++ src/main/java/spring/backend/activity/presentation/swagger/ReadMonthlyActivityOverviewSwagger.java
@@ -0,0 +1,20 @@
+package spring.backend.activity.presentation.swagger;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import spring.backend.activity.dto.request.MonthlyActivityOverviewRequest;
+import spring.backend.activity.dto.response.MonthlyActivityOverviewResponse;
+import spring.backend.core.presentation.RestResponse;
+import spring.backend.member.domain.entity.Member;
+
+@Tag(name = "Activity" , description = "활동")
+public interface ReadMonthlyActivityOverviewSwagger {
+    @Operation(
+            summary = "월간 활동 개요 조회 API",
+            description = "사용자의 월간 활동 개요를 조회합니다.",
+            operationId = "/v1/activity"
+    )
+    ResponseEntity<RestResponse<MonthlyActivityOverviewResponse>> readMonthlyActivityOverviewController(@Parameter(hidden = true) Member member, MonthlyActivityOverviewRequest monthlyActivityOverviewRequest);
+}

8ee57f5674b441c866f669dd85cf556987bc636c
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-17 02:42:16 +0900

    chore: (#98) 예외 발생 시 Sentry에 기록한다.

--- src/main/java/spring/backend/core/exception/GlobalExceptionHandler.java
+++ src/main/java/spring/backend/core/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package spring.backend.core.exception;
 
+import io.sentry.Sentry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -21,6 +22,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
   @ExceptionHandler(Exception.class)
   public final ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex, WebRequest request) {
     log.error("ERROR ::: [AllException] ", ex);
+    Sentry.captureException(ex);
     ErrorResponse errorResponse = ErrorResponse.createErrorResponse().statusCode(500).exception(ex).build();
     return ResponseEntity.internalServerError().body(errorResponse);
   }
@@ -29,6 +31,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
   public final ResponseEntity<ErrorResponse> handleDomainException(DomainException ex) {
     HttpStatus httpStatus = Optional.ofNullable(ex.getHttpStatus()).orElse(HttpStatus.INTERNAL_SERVER_ERROR);
     log.error("ERROR ::: [DomainException] ", ex);
+    Sentry.captureException(ex);
     ErrorResponse errorResponse = ErrorResponse.createDomainErrorResponse().statusCode(httpStatus.value()).exception(ex).build();
     return ResponseEntity.status(httpStatus).body(errorResponse);
   }
@@ -36,6 +39,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
   @Override
   protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
     log.error("ERROR ::: [MethodArgumentNotValidException] ", ex);
+    Sentry.captureException(ex);
     ErrorResponse errorResponse = ErrorResponse.createValidationErrorResponse().statusCode(400).exception(ex).build();
     return ResponseEntity.badRequest().body(errorResponse);
   }

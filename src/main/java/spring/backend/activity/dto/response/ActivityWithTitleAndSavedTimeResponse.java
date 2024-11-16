2e273279b2548bca9d91db15c4f14f50f2001bfc
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-17 00:46:48 +0900

    feat: (#95) 이번 달 키워드 활동 조회를 위한 request dto와 response dto를 생성한다.

---
+++ src/main/java/spring/backend/activity/dto/response/ActivityWithTitleAndSavedTimeResponse.java
@@ -0,0 +1,19 @@
+package spring.backend.activity.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.time.LocalDateTime;
+
+public record ActivityWithTitleAndSavedTimeResponse(
+        @Schema(description = "활동 제목", example = "마음의 편안을 가져다주는 명상음악 20분 듣기")
+        String title,
+
+        @Schema(description = "모은 시간", example = "60")
+        int savedTime,
+
+        @Schema(description = "활동 날짜", example = "2021-07-01T00:00:00")
+        @JsonProperty("dateOfActivity")
+        LocalDateTime createdAt
+) {
+}

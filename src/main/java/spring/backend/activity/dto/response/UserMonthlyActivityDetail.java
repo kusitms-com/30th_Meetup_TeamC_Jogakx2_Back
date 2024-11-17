6f3d65035befb817b23c482f446a07e425e30b4e
anxi01 <tjdals2893@naver.com>
2024-11-17 18:26:31 +0900

    feat: (#90) 활동 캘린더 조회 API 스웨거를 작성한다

--- src/main/java/spring/backend/activity/dto/response/UserMonthlyActivityDetail.java
+++ src/main/java/spring/backend/activity/dto/response/UserMonthlyActivityDetail.java
@@ -1,17 +1,23 @@
 package spring.backend.activity.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import spring.backend.activity.domain.value.Keyword.Category;
 
 import java.time.LocalDateTime;
 
 public record UserMonthlyActivityDetail(
 
+        @Schema(description = "활동 카테고리 \n\n SELF_DEVELOPMENT(자기개발), HEALTH(건강), NATURE(자연), CULTURE_ART(문화/예술), ENTERTAINMENT(엔터테인먼트), RELAXATION(휴식), SOCIAL(소셜)",
+                example = "SELF_DEVELOPMENT")
         Category category,
 
+        @Schema(description = "활동 제목", example = "마음의 편안을 가져다주는 명상음악 20분 듣기")
         String title,
 
+        @Schema(description = "모은 시간", example = "20")
         int savedTime,
 
+        @Schema(description = "활동 생성 시간", example = "2024-11-16T14:24:08.548712")
         LocalDateTime activityCreatedAt
 ) {
 }
---
+++ src/main/java/spring/backend/activity/presentation/swagger/ReadActivityCalendarSwagger.java
@@ -0,0 +1,26 @@
+package spring.backend.activity.presentation.swagger;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.http.ResponseEntity;
+import spring.backend.activity.dto.request.ReadActivityCalendarRequest;
+import spring.backend.activity.dto.response.ActivityCalendarResponse;
+import spring.backend.activity.exception.ActivityErrorCode;
+import spring.backend.core.configuration.swagger.ApiErrorCode;
+import spring.backend.core.exception.error.GlobalErrorCode;
+import spring.backend.core.presentation.RestResponse;
+import spring.backend.member.domain.entity.Member;
+
+@Tag(name = "Activity", description = "활동")
+public interface ReadActivityCalendarSwagger {
+
+    @Operation(
+            summary = "활동 캘린더 조회 API",
+            description = "사용자가 연월을 선택하여 월별 활동 요약과 월별 활동 상세 정보 리스트를 반환합니다.",
+            operationId = "/v1/activity-calendar"
+    )
+    @ApiErrorCode({GlobalErrorCode.class, ActivityErrorCode.class})
+    ResponseEntity<RestResponse<ActivityCalendarResponse>> readActivityCalendar(@Parameter(hidden = true) Member member, @ParameterObject ReadActivityCalendarRequest request);
+}

049911de7f2ab7f048174a6feb13a3fc659e576e
anxi01 <tjdals2893@naver.com>
2024-11-19 07:54:19 +0900

    feat: (#102) OpenAI 추천 API 스웨거를 작성한다

--- src/main/java/spring/backend/recommendation/dto/response/OpenAIRecommendationResponse.java
+++ src/main/java/spring/backend/recommendation/dto/response/OpenAIRecommendationResponse.java
@@ -1,17 +1,23 @@
 package spring.backend.recommendation.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import spring.backend.activity.domain.value.Keyword.Category;
 
 public record OpenAIRecommendationResponse(
 
+        @Schema(description = "추천 결과 순서", example = "1")
         int order,
 
+        @Schema(description = "직접적 추천 활동", example = "마음의 편안을 가져다주는 명상 음악 20분 듣기")
         String title,
 
+        @Schema(description = "꾸밈글", example = "휴식에는 역시 명상이 최고!")
         String content,
 
+        @Schema(description = "활동 키워드", example = "[\"NATURE\",\"CULTURE_ART\"]")
         Category keywordCategory,
 
+        @Schema(description = "외부 링크", example = "https://www.youtube.com")
         String url
 ) {
     public static OpenAIRecommendationResponse of(int order, String title, String content, Category category, String url) {

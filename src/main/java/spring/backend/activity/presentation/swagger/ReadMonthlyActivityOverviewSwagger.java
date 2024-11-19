9a6427ad4d438f81f71c5ed03e0816de28716315
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-19 14:50:04 +0900

    refactor: (#87) swagger 엔드포인트를 변경한다.

--- src/main/java/spring/backend/activity/presentation/swagger/ReadMonthlyActivityOverviewSwagger.java
+++ src/main/java/spring/backend/activity/presentation/swagger/ReadMonthlyActivityOverviewSwagger.java
@@ -9,12 +9,12 @@ import spring.backend.activity.dto.response.MonthlyActivityOverviewResponse;
 import spring.backend.core.presentation.RestResponse;
 import spring.backend.member.domain.entity.Member;
 
-@Tag(name = "Activity" , description = "활동")
+@Tag(name = "Activity", description = "활동")
 public interface ReadMonthlyActivityOverviewSwagger {
     @Operation(
             summary = "월간 활동 개요 조회 API",
             description = "사용자의 월간 활동 개요를 조회합니다.",
-            operationId = "/v1/activity"
+            operationId = "/v1/activities/overview"
     )
     ResponseEntity<RestResponse<MonthlyActivityOverviewResponse>> readMonthlyActivityOverview(@Parameter(hidden = true) Member member, MonthlyActivityOverviewRequest monthlyActivityOverviewRequest);
 }

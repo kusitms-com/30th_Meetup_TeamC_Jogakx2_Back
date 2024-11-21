d96d3686a74f72f5a73c25839e2813ce3552a897
anxi01 <tjdals2893@naver.com>
2024-11-22 06:25:19 +0900

    feat: (#109) 시간 범위 내 빠른 시작을 사용자별 1개씩 가져오는 JPQL을 추가한다

--- src/main/java/spring/backend/activity/infrastructure/persistence/jpa/repository/QuickStartJpaRepository.java
+++ src/main/java/spring/backend/activity/infrastructure/persistence/jpa/repository/QuickStartJpaRepository.java
@@ -1,7 +1,24 @@
 package spring.backend.activity.infrastructure.persistence.jpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import spring.backend.activity.infrastructure.persistence.jpa.entity.QuickStartJpaEntity;
 
+import java.time.LocalTime;
+import java.util.List;
+
 public interface QuickStartJpaRepository extends JpaRepository<QuickStartJpaEntity, Long> {
+
+    @Query("""
+        select q
+        from QuickStartJpaEntity q
+        where q.startTime between :lowerBound and :upperBound
+        and q.id in (
+            select min(q2.id)
+            from QuickStartJpaEntity q2
+            where q2.memberId = q.memberId
+            group by q2.memberId
+        )
+    """)
+    List<QuickStartJpaEntity> findQuickStartsWithinTimeRange(LocalTime lowerBound, LocalTime upperBound);
 }

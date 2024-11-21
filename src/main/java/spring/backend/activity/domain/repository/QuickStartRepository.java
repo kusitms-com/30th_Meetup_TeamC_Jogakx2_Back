c2220441fb36485afd31ae26005b882ae1d83315
anxi01 <tjdals2893@naver.com>
2024-11-22 07:23:52 +0900

    feat: (#109) Scheduler를 이용해서 15분 주기로 빠른시작 알림 메일을 전송한다

--- src/main/java/spring/backend/activity/domain/repository/QuickStartRepository.java
+++ src/main/java/spring/backend/activity/domain/repository/QuickStartRepository.java
@@ -2,8 +2,12 @@ package spring.backend.activity.domain.repository;
 
 import spring.backend.activity.domain.entity.QuickStart;
 
+import java.time.LocalTime;
+import java.util.List;
+
 public interface QuickStartRepository {
 
     QuickStart findById(Long id);
     QuickStart save(QuickStart quickStart);
+    List<QuickStart> findQuickStartsWithinTimeRange(LocalTime lowerBound, LocalTime upperBound);
 }

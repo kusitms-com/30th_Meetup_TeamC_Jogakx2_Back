9ab5076ed632e6330582b1a9afc0ce99ea12dfba
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-22 15:24:06 +0900

    refactor: (#110) 토큰 재발급시 새로운 accessToken을 쿠키에 넣어준다.

--- src/main/java/spring/backend/auth/presentation/RotateAccessTokenController.java
+++ src/main/java/spring/backend/auth/presentation/RotateAccessTokenController.java
@@ -1,9 +1,12 @@
 package spring.backend.auth.presentation;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.backend.auth.application.RotateAccessTokenService;
@@ -13,13 +16,22 @@ import spring.backend.core.presentation.RestResponse;
 @RestController
 @RequestMapping("/v1/token/rotate")
 @RequiredArgsConstructor
+@Log4j2
 public class RotateAccessTokenController {
     private final RotateAccessTokenService rotateTokenService;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<RestResponse<RotateAccessTokenResponse>> rotateAccessToken(
-            @CookieValue(name = "refreshToken", required = false) String refreshToken
+            @CookieValue(name = "access_token", required = false) String accessToken
     ) {
-        return ResponseEntity.ok(new RestResponse<>(rotateTokenService.rotateAccessToken(refreshToken)));
+        RotateAccessTokenResponse rotateAccessTokenResponse = rotateTokenService.rotateAccessToken(accessToken);
+        ResponseCookie cookie = ResponseCookie.from("access_token", rotateAccessTokenResponse.accessToken())
+                .httpOnly(true)
+                .path("/")
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 }

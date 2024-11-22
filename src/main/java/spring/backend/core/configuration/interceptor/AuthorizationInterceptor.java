7b419de63cfac7ced6d0c47657fafb4dc07e8f3f
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-22 17:09:15 +0900

    feat: (#110) 인증 우회 uri를 설정한다.

--- src/main/java/spring/backend/core/configuration/interceptor/AuthorizationInterceptor.java
+++ src/main/java/spring/backend/core/configuration/interceptor/AuthorizationInterceptor.java
@@ -10,6 +10,9 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import spring.backend.auth.exception.AuthenticationErrorCode;
 import spring.backend.core.application.JwtService;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 @Log4j2
@@ -17,12 +20,17 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
 
     private final JwtService jwtService;
 
+    private static final List<String> PASS_THROUGH_PATTERNS = Arrays.asList(
+            "/swagger-ui", "/v3/api-docs", "/v1/oauth", "/v1/token/rotate"
+    );
+
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        if (isOAuthLoginRequest(request)) {
+
+        if (isPassThroughRequest(request.getRequestURI())) {
             return true;
         }
-
         String accessToken = extractToken(request);
         if (accessToken == null) {
             log.error("쿠키에 토큰이 존재하지 않습니다.");
@@ -32,14 +40,15 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
         return true;
     }
 
-    private boolean isOAuthLoginRequest(HttpServletRequest request) {
-        return request.getRequestURI().startsWith("/v1/oauth");
+    private boolean isPassThroughRequest(String uri) {
+        return PASS_THROUGH_PATTERNS.stream().anyMatch(uri::contains);
     }
 
     private String extractToken(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
+                log.info("cookie: {}", cookie);
                 if ("access_token".equals(cookie.getName())) {
                     return cookie.getValue();
                 }

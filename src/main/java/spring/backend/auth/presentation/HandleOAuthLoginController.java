500f5e0732deb8da27dc5736143ca95e58bf765e
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-22 15:08:12 +0900

    refactor: (#110) 로그인 성공 시 쿠키에 토큰과 유저정보를 넣어준다.

--- src/main/java/spring/backend/auth/presentation/HandleOAuthLoginController.java
+++ src/main/java/spring/backend/auth/presentation/HandleOAuthLoginController.java
@@ -1,11 +1,12 @@
 package spring.backend.auth.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import spring.backend.auth.application.HandleOAuthLoginService;
 import spring.backend.auth.dto.response.LoginResponse;
-import spring.backend.core.presentation.RestResponse;
 
 @RestController
 @RequestMapping("/v1/oauth/login")
@@ -15,10 +16,20 @@ public class HandleOAuthLoginController {
     private final HandleOAuthLoginService handleOAuthLoginService;
 
     @GetMapping("/{providerName}")
-    public ResponseEntity<RestResponse<LoginResponse>> handleOAuthLogin(@RequestParam(value = "code", required = false) String code,
-                                                                        @RequestParam(value = "state", required = false) String state,
-                                                                        @PathVariable String providerName) {
+    public ResponseEntity<?> handleOAuthLogin(@RequestParam(value = "code", required = false) String code,
+                                              @RequestParam(value = "state", required = false) String state, @PathVariable String providerName) {
         LoginResponse loginResponse = handleOAuthLoginService.handleOAuthLogin(providerName, code, state);
-        return ResponseEntity.ok(new RestResponse<>(loginResponse));
+        ResponseCookie accessTokenCookie = ResponseCookie.from("access_token", loginResponse.accessToken())
+                .httpOnly(true)
+                .build();
+
+        ResponseCookie roleCookie = ResponseCookie.from("user_role", loginResponse.role().toString())
+                .httpOnly(true)
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, accessTokenCookie.toString())
+                .header(HttpHeaders.SET_COOKIE, roleCookie.toString())
+                .build();
     }
 }

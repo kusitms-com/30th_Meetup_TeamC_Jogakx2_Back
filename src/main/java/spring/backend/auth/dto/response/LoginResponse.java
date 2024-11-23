29d4106dde2fff5453c851481fa5661340211a10
anxi01 <tjdals2893@naver.com>
2024-11-24 05:06:31 +0900

    fix: (#135) 로그인 응답에 사용자 가입 날짜를 추가한다

--- src/main/java/spring/backend/auth/dto/response/LoginResponse.java
+++ src/main/java/spring/backend/auth/dto/response/LoginResponse.java
@@ -1,6 +1,25 @@
 package spring.backend.auth.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import spring.backend.member.domain.value.Role;
 
-public record LoginResponse(String accessToken, String refreshToken, Role role) {
+import java.time.LocalDateTime;
+
+public record LoginResponse(
+
+        @Schema(description = "액세스 토큰")
+        String accessToken,
+
+        @Schema(description = "리프레시 토큰")
+        String refreshToken,
+
+        @Schema(description = "사용자 유형(MEMBER, GUEST)", example = "MEMBER")
+        Role role,
+
+        @Schema(description = "사용자 가입 날짜", example = "2024-11-14T06:10:55.091954")
+        LocalDateTime registrationDate
+) {
+    public static LoginResponse of(String accessToken, String refreshToken, Role role, LocalDateTime registrationDate) {
+        return new LoginResponse(accessToken, refreshToken, role, registrationDate);
+    }
 }

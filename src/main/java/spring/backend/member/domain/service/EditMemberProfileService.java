c592f1bf5a05692160ef2952ee581782292fd8ea
HoyeongJeon <hoyeong0302@gmail.com>
2024-11-23 22:18:16 +0900

    feat: (#126) 닉네임 유효성 검사를 ValidateNicknameService에 위임한다.

--- src/main/java/spring/backend/member/domain/service/EditMemberProfileService.java
+++ src/main/java/spring/backend/member/domain/service/EditMemberProfileService.java
@@ -3,6 +3,7 @@ package spring.backend.member.domain.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import spring.backend.member.application.ValidateNicknameService;
 import spring.backend.member.domain.entity.Member;
 import spring.backend.member.domain.repository.MemberRepository;
 import spring.backend.member.dto.request.EditMemberProfileRequest;
@@ -14,39 +15,12 @@ import spring.backend.member.exception.MemberErrorCode;
 public class EditMemberProfileService {
 
     private final MemberRepository memberRepository;
+    private final ValidateNicknameService validateNicknameService;
 
     public boolean edit(Member member, EditMemberProfileRequest editMemberProfileRequest) {
-        try {
-            if (validateNickname(editMemberProfileRequest.nickname())) {
-                member.editMemberProfile(editMemberProfileRequest.nickname(), editMemberProfileRequest.profileImage());
-                memberRepository.save(member);
-                return true;
-            } else {
-                return false;
-            }
-        } catch (Exception e) {
-            log.error("[EditMemberProfileService] Failed to edit member profile. {}", e.getMessage());
-            throw MemberErrorCode.FAILED_TO_EDIT_PROFILE.toException();
-        }
-    }
-
-    private boolean validateNickname(String nickname) {
-        if (nickname == null || nickname.isBlank()) {
-            log.error("[ValidateNicknameService] Nickname is empty");
-            return false;
-        }
-
-        if (nickname.length() > 6) {
-            log.error("[ValidateNicknameService] Nickname is smaller than 6 characters");
-            return false;
-        }
-
-        if (!nickname.matches("^[a-zA-Z0-9가-힣]+$")) {
-            log.error("[ValidateNicknameService] Nickname is invalid");
-            return false;
-        }
-
+        validateNicknameService.validateNickname(editMemberProfileRequest.nickname());
+        member.editMemberProfile(editMemberProfileRequest.nickname(), editMemberProfileRequest.profileImage());
+        memberRepository.save(member);
         return true;
     }
 }
-

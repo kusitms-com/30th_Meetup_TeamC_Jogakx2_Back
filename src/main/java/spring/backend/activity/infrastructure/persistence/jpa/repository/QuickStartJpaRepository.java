31c00d2479e3215efc11f59e42f2576b05acf0d8
anxi01 <tjdals2893@naver.com>
2024-11-28 01:58:06 +0900

    feat: (#151) 사용자와 빠른 시작 정보를 가지고 메일 제목을 커스텀할 수 있다

--- src/main/java/spring/backend/activity/infrastructure/persistence/jpa/repository/QuickStartJpaRepository.java
+++ src/main/java/spring/backend/activity/infrastructure/persistence/jpa/repository/QuickStartJpaRepository.java
@@ -1,7 +1,21 @@
 package spring.backend.activity.infrastructure.persistence.jpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import spring.backend.activity.infrastructure.persistence.jpa.entity.QuickStartJpaEntity;
 
+import java.time.LocalTime;
+import java.util.List;
+import java.util.UUID;
+
 public interface QuickStartJpaRepository extends JpaRepository<QuickStartJpaEntity, Long> {
+
+    @Query("""
+        select q
+        from QuickStartJpaEntity q
+        where q.memberId in :memberIds
+        and q.startTime between :lowerBound and :upperBound
+        order by q.startTime asc
+    """)
+    List<QuickStartJpaEntity> findEarliestQuickStartsForMembers(List<UUID> memberIds, LocalTime lowerBound,LocalTime upperBound);
 }

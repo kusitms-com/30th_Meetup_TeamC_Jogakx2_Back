2199223123706bae8dd251ec1e30fb554dc13835
anxi01 <tjdals2893@naver.com>
2024-11-28 14:54:22 +0900

    fix: (#160) 조각 별 상세 정보가 아닌 활동 키워드에서 키워드 이미지는 투명도 30으로 변경한다

--- src/main/java/spring/backend/activity/application/ReadMonthlyActivityOverviewService.java
+++ src/main/java/spring/backend/activity/application/ReadMonthlyActivityOverviewService.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import spring.backend.activity.domain.value.Keyword.Category;
 import spring.backend.activity.dto.request.MonthlyActivityOverviewRequest;
 import spring.backend.activity.dto.response.MonthlyActivityCountByKeywordResponse;
 import spring.backend.activity.dto.response.MonthlyActivityOverviewResponse;
 import spring.backend.activity.dto.response.MonthlySavedTimeAndActivityCountResponse;
+import spring.backend.activity.infrastructure.persistence.jpa.value.KeywordJpaValue;
 import spring.backend.activity.query.dao.ActivityDao;
+import spring.backend.core.converter.ImageConverter;
 import spring.backend.core.util.TimeUtil;
 import spring.backend.member.domain.entity.Member;
 
@@ -24,12 +27,22 @@ public class ReadMonthlyActivityOverviewService {
 
     private final ActivityDao activityDao;
 
+    private final ImageConverter imageConverter;
+
     public MonthlyActivityOverviewResponse readMonthlyActivityOverview(Member member, MonthlyActivityOverviewRequest monthlyActivityOverviewRequest) {
         YearMonth yearMonth = YearMonth.of(monthlyActivityOverviewRequest.year(), monthlyActivityOverviewRequest.month());
         LocalDateTime startDayOfMonth = TimeUtil.toStartDayOfMonth(yearMonth);
         LocalDateTime endDayOfMonth = TimeUtil.toEndDayOfMonth(yearMonth);
         MonthlySavedTimeAndActivityCountResponse monthlySavedTimeAndActivityCountResponse = activityDao.findMonthlyTotalSavedTimeAndTotalCount(member.getId(), startDayOfMonth, endDayOfMonth);
         List<MonthlyActivityCountByKeywordResponse> activityByKeywordSummaryResponses = activityDao.findMonthlyActivitiesByKeywordSummary(member.getId(), startDayOfMonth, endDayOfMonth);
-        return new MonthlyActivityOverviewResponse(member.getUpdatedAt().getYear(), member.getUpdatedAt().getMonth(), monthlySavedTimeAndActivityCountResponse, activityByKeywordSummaryResponses);
+        List<MonthlyActivityCountByKeywordResponse> updatedActivityByKeywordSummaryResponses = activityByKeywordSummaryResponses.stream()
+                .map(response -> {
+                    Category category = response.keyword().getCategory();
+                    String imageUrl = imageConverter.convertToTransparent30ImageUrl(category);
+                    KeywordJpaValue updatedKeyword = KeywordJpaValue.create(category, imageUrl);
+                    return new MonthlyActivityCountByKeywordResponse(updatedKeyword, response.activityCount());
+                })
+                .toList();
+        return new MonthlyActivityOverviewResponse(member.getUpdatedAt().getYear(), member.getUpdatedAt().getMonth(), monthlySavedTimeAndActivityCountResponse, updatedActivityByKeywordSummaryResponses);
     }
 }
